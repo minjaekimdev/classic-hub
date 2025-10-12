402eab4c79d9e8f24686d66f2032685956cbfe7d
MJKIM <minjaekimm1@gmail.com>
2025-10-12 21:42:30 +0900

    fix: daily-tasks-runner 함수 즉시 실행되도록 변경

--- backend/database/cron/daily-tasks-runner.ts
+++ backend/database/cron/daily-tasks-runner.ts
@@ -1,11 +1,11 @@
-import { updateAllRankingData } from "./ranking-performance"
-import { updatePerformanceData } from './all-performance';
+import { updateAllRankingData } from "./ranking-performance";
+import { updatePerformanceData } from "./all-performance";
 
-async function main() {
+(async function main() {
   console.log("cron 작업을 시작합니다...");
-  
+
   await updateAllRankingData();
   await updatePerformanceData();
-  
+
   console.log("모든 작업이 완료되었습니다!");
-}
+})();
