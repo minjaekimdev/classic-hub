4b181bf67692764d7dbeafc7680c5187011db80f
MJKIM <minjaekimm1@gmail.com>
2026-01-19 17:06:53 +0900

    refactor: DB 접근방식 anon key에서 service key로 변경

--- packages/backend/infrastructure/external-api/supabase.ts
+++ packages/backend/infrastructure/external-api/supabase.ts
@@ -3,11 +3,11 @@ import "dotenv/config";
 
 const supabaseUrl = "https://razltwjkvwqiyksaydcw.supabase.co";
 
-if (!process.env.SUPABASE_ANON_KEY) {
+if (!process.env.SUPABASE_SERVICE_ROLE_KEY) {
   throw new Error("환경변수 SUPABASE_ANON_KEY가 설정되어 있지 않습니다!");
 }
 
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
 export default supabase;
