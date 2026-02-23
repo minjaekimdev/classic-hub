7e1a273c81dd8e8f137733784c991acb243a0ce4
MJKIM <minjaekimm1@gmail.com>
2026-02-23 13:20:41 +0900

    refactor: 결과 페이지에서 데이터를 필터링하는 useFilteredPerformances 훅 useMemo로 메모이제이션 적용

--- packages/frontend/src/features/filter/hooks/useFilteredPerformances.ts
+++ packages/frontend/src/features/filter/hooks/useFilteredPerformances.ts
@@ -1,15 +1,20 @@
 import type { DetailPerformance } from "@classic-hub/shared/types/client";
+import { useMemo } from "react";
 
 const useFilteredPerformances = (
   performances: DetailPerformance[],
   selectedVenues: string[],
 ) => {
-  const selectedVenueIdSet = new Set(selectedVenues);
-  if (selectedVenueIdSet.size === 0) {
-    return performances;
-  }
+  const result = useMemo(() => {
+    const selectedVenueIdSet = new Set(selectedVenues);
+    if (selectedVenueIdSet.size === 0) {
+      return performances;
+    }
 
-  return performances.filter((perf) => selectedVenueIdSet.has(perf.venueId!));
+    return performances.filter((perf) => selectedVenueIdSet.has(perf.venueId!));
+  }, [performances, selectedVenues]);
+
+  return result;
 };
 
 export default useFilteredPerformances;
