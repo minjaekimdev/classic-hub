62dbeae27aea5b310dba1f36527683a2e1f01476
MJKIM <minjaekimm1@gmail.com>
2026-03-17 20:35:48 +0900

    refactor: 에러 처리 로직 개선

---
+++ packages/backend/application/use-cases/kopis/getPerformanceIds.test.ts
@@ -0,0 +1,16 @@
+import RateLimiter from "@/shared/utils/rateLimiter";
+import { describe, it } from "vitest";
+import { getPerformanceIds } from "./getPerformanceIds";
+import { KOPIS_RATE_LIMIT } from "@/application/constants/rateLimits";
+
+describe("getPerformanceIds 테스트", () => {
+  it("여러 페이지의 공연 데이터를 정상적으로 가져와야 한다", async () => {
+    const startDate = "20260316";
+    const endDate = "20260323";
+    const kopisRateLimiter = new RateLimiter(KOPIS_RATE_LIMIT);
+
+    const performanceIds = await getPerformanceIds(startDate, endDate, kopisRateLimiter)
+    
+    console.log("performanceIds: ", performanceIds);
+  })
+})
