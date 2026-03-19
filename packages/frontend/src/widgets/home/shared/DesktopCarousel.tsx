300bc0b56182d4d681c09f22b47bb1df17d8fc13
MJKIM <minjaekimm1@gmail.com>
2026-03-19 18:22:49 +0900

    fix: 캐러셀 네비게이션 중복 호출 해결 및 이동 단위 조정

--- packages/frontend/src/widgets/home/shared/DesktopCarousel.tsx
+++ packages/frontend/src/widgets/home/shared/DesktopCarousel.tsx
@@ -1,7 +1,6 @@
-import { useId, useState } from "react";
+import { useId } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
-import { Swiper as SwiperType } from "swiper";
 import type { SwiperOptions } from "swiper/types"; // 타입 지원을 위해
 import leftArrow from "@shared/assets/icons/left-slidearrow-black.svg";
 import rightArrow from "@shared/assets/icons/right-slidearrow-black.svg";
@@ -12,6 +11,7 @@ interface DesktopCarouselProps<T> {
   renderItem: (item: T) => React.ReactNode;
   breakpoints?: SwiperOptions["breakpoints"];
   slidesPerView?: number | "auto";
+  slidesPerGroup?: number;
   spaceBetween?: number;
 }
 
@@ -20,9 +20,9 @@ export const DesktopCarousel = <T extends BaseItem>({
   renderItem,
   breakpoints,
   slidesPerView = 4, // 기본값
+  slidesPerGroup = 1, // 기본값
   spaceBetween = 21,
 }: DesktopCarouselProps<T>) => {
-  const [swiperInstance, setSwiperInstance] = useState<SwiperType | null>(null);
   const uniqueId = useId().replace(/:/g, "");
   const prevClass = `prev-${uniqueId}`;
   const nextClass = `next-${uniqueId}`;
@@ -31,16 +31,15 @@ export const DesktopCarousel = <T extends BaseItem>({
     <div className="relative w-full">
       <button
         className={`${prevClass} hidden desktop:flex absolute z-10 top-[50%] left-0 w-13 h-13 justify-center items-center border border-[rgba(0,0,0,0.1)] rounded-full bg-[rgba(255,255,255,0.9)] shadow-[0_0_8px_0_rgba(0,0,0,0.13)] translate-y-[-50%] translate-x-[-50%] disabled:opacity-0 transition-opacity`}
-        onClick={() => swiperInstance?.slidePrev()}
       >
         <img src={leftArrow} alt="이전" />
       </button>
       <Swiper
         modules={[Navigation]}
         navigation={{ prevEl: `.${prevClass}`, nextEl: `.${nextClass}` }}
-        onSwiper={setSwiperInstance}
         spaceBetween={spaceBetween}
         slidesPerView={slidesPerView}
+        slidesPerGroup={slidesPerGroup}
         breakpoints={breakpoints}
         allowTouchMove={true} // 모바일 대응을 위해 true 권장
       >
@@ -50,7 +49,6 @@ export const DesktopCarousel = <T extends BaseItem>({
       </Swiper>
       <button
         className={`${nextClass} hidden desktop:flex absolute z-10 top-[50%] right-0 w-13 h-13 justify-center items-center border border-[rgba(0,0,0,0.1)] rounded-full bg-[rgba(255,255,255,0.9)] shadow-[0_0_8px_0_rgba(0,0,0,0.13)] translate-y-[-50%] translate-x-[50%] disabled:opacity-0 transition-opacity`}
-        onClick={() => swiperInstance?.slideNext()}
       >
         <img src={rightArrow} alt="다음" />
       </button>
