b20002ca13b009b5d1020c7dd554977ee4eaefa7
MJKIM <minjaekimm1@gmail.com>
2026-03-20 19:31:40 +0900

    refactor: 프론트엔드 기간 필터 로직 수정, formatDateRange 함수 변경된 백엔드 날짜 형식에 맞춰 변경

--- packages/frontend/src/shared/utils/formatDateRange.ts
+++ packages/frontend/src/shared/utils/formatDateRange.ts
@@ -1,13 +1,16 @@
 const formatDateRange = (startDate: string | null, endDate: string | null) => {
   if (startDate === null || endDate === null) return "";
   const days = ["일", "월", "화", "수", "목", "금", "토"];
-  const startDayIdx = new Date(startDate.replaceAll(".", "-")).getDay();
-  const endDayIdx = new Date(endDate.replaceAll(".", "-")).getDay();
+  const startDayIdx = new Date(startDate).getDay();
+  const endDayIdx = new Date(endDate).getDay();
+
+  const parsedStartDate = startDate.replaceAll("-", ".");
+  const parsedEndDate = endDate.replaceAll("-", ".");
 
   if (startDate === endDate) {
-    return `${startDate}(${days[startDayIdx]})`;
+    return `${parsedStartDate}(${days[startDayIdx]})`;
   }
-  return `${startDate}(${days[startDayIdx]}) ~ ${endDate}(${days[endDayIdx]})`;
+  return `${parsedStartDate}(${days[startDayIdx]}) ~ ${parsedEndDate}(${days[endDayIdx]})`;
 };
 
 export default formatDateRange;
