017a2684225e2f2ef2761667ccde95d7ab027305
MJKIM <minjaekimm1@gmail.com>
2026-04-03 23:08:04 +0900

    config: Sentry 환경 구축

--- packages/frontend/src/app/main.tsx
+++ packages/frontend/src/app/main.tsx
@@ -2,6 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@app/styles/main.css";
 import App from "./App";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://aaa9941a4ef812ffefc0f4422674dcfc@o4511156351795200.ingest.us.sentry.io/4511156353236992",
+  sendDefaultPii: true,
+
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
