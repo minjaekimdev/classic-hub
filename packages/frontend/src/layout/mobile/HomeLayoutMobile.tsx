db87a6e76b6b6f7826719e12349dbe312a8b0b94
MJKIM <minjaekimm1@gmail.com>
2026-04-03 17:59:38 +0900

    chore: 랭킹 페이지 모바일 레이아웃 수정

--- packages/frontend/src/layout/mobile/HomeLayoutMobile.tsx
+++ packages/frontend/src/layout/mobile/HomeLayoutMobile.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import useHomeLayoutMobile from "../hooks/useHomeLayoutMobile";
-import BottomSheet from "@/shared/ui/bottom-sheet/BottomSheet";
 import HomeHeaderMobile from "@/widgets/home/ui/HomeHeaderMobile";
 
 const HomeLayoutMobile = ({ children }: { children: React.ReactNode }) => {
   const { ref, isScrollZero, marginTop } = useHomeLayoutMobile();
   return (
-    <BottomSheet>
+    <>
       <HomeHeaderMobile isScrollZero={isScrollZero} />
       <div ref={ref} className="h-1 bg-transparent"></div>
       <main className={`pt-6 ${marginTop}`}>{children}</main>
-    </BottomSheet>
+    </>
   );
 };
 
--- packages/frontend/src/pages/Ranking.tsx
+++ packages/frontend/src/pages/Ranking.tsx
@@ -4,6 +4,26 @@ import RankingHeader from "@/widgets/ranking/RankingHeader";
 import RankList from "@/widgets/ranking/RankList";
 import LayoutDesktop from "@/layout/desktop/LayoutDesktop";
 import { ModalProvider } from "@/app/providers/modal/ModalProvider";
+import useBreakpoint from "@/shared/hooks/useBreakpoint";
+import { BREAKPOINTS } from "@/shared/constants";
+
+const LayoutSwitcher = ({ children }: { children: React.ReactNode }) => {
+  const isMobile = useBreakpoint(BREAKPOINTS.MOBILE);
+
+  if (!isMobile) {
+    return (
+      <LayoutDesktop variant="main">
+        <LayoutDesktop.Wrapper hasPaddingTop={true}>
+          <div className="flex items-start">
+            <div className="flex-1">{children}</div>
+          </div>
+        </LayoutDesktop.Wrapper>
+      </LayoutDesktop>
+    );
+  } else {
+    return <div className="pt-8">{children}</div>;
+  }
+};
 
 const Ranking = () => {
   const [period, setPeriod] = useState<Period>("daily");
@@ -14,14 +34,12 @@ const Ranking = () => {
 
   return (
     <ModalProvider>
-      <LayoutDesktop variant="main">
-        <LayoutDesktop.Wrapper hasPaddingTop={true}>
-          <div className="flex flex-col gap-[2.19rem] px-4">
-            <RankingHeader period={period} onToggle={handlePeriod} />
-            <RankList period={period} />
-          </div>
-        </LayoutDesktop.Wrapper>
-      </LayoutDesktop>
+      <LayoutSwitcher>
+        <div className="flex flex-col gap-[2.19rem] px-4">
+          <RankingHeader period={period} onToggle={handlePeriod} />
+          <RankList period={period} />
+        </div>
+      </LayoutSwitcher>
     </ModalProvider>
   );
 };
