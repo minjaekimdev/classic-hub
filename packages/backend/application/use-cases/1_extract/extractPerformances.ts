e3815caa4dbdf7e3c7ffe309edf9bcea687ed426
MJKIM <minjaekimm1@gmail.com>
2026-06-03 19:00:48 +0900

    refactor: extract단계 compareNewOld의 외부 의존성을 extractPerformances로 이동

--- packages/backend/application/use-cases/1_extract/extractPerformances.ts
+++ packages/backend/application/use-cases/1_extract/extractPerformances.ts
@@ -3,7 +3,8 @@ import { Dayjs } from "dayjs";
 import { compareNewOld } from "./modules/compareNewOld";
 import { getPerformanceIds } from "./modules/getPerformanceIds";
 import { getPerformanceList } from "./modules/getPerformanceList";
-import { deleteOldPerformances } from "./modules/deleteOldPerformances";
+import { getColumnData } from "@/infrastructure/external-api/supabase/database";
+import { deletePerformances } from "./modules/deletePerformances";
 
 export const extractPerformances = async (
   now: Dayjs,
@@ -12,34 +13,44 @@ export const extractPerformances = async (
   afterDate: string,
   updateEndDate: string,
 ) => {
+  // 로그를 현재 단계, KOPIS페칭, DB관련 로직에 따라 각각 PROCESS, KOPIS, DB로 분류
   logger.info(
-    `Fetching new performance datas at ${now.format("YYYYMMDD")} (target period: ${startDate} ~ ${endDate})`,
+    `[PROCESS] 새로운 공연 데이터 동기화 시작 (대상 기간: ${startDate} ~ ${endDate})`,
   );
   // 1) 새로운 데이터를 페칭
-  const newPerformanceIds = await getPerformanceIds(startDate, endDate);
-  console.log(newPerformanceIds);
+  const newIds = await getPerformanceIds(startDate, endDate);
+  logger.info("[KOPIS] 새로운 공연 ID:", newIds);
+
+  const dbIds = await getColumnData("performances", "performance_id");
+  logger.info("[DB] DB에 존재하는 공연 ID:", dbIds);
 
   // 2) DB와 새로운 데이터를 비교하여 삭제할 데이터와 삽입할 데이터의 id를 가져오기
-  logger.info("Comparing new datas with DB...");
-  const { idsToDelete, idsToInsert } = await compareNewOld(newPerformanceIds);
+  logger.info("[PROCESS] DB에 존재하는 공연 ID와 새로운 공연 ID 비교");
+  const { idsToDelete, idsToInsert } = compareNewOld(newIds, dbIds);
 
   // 3) 데이터 삭제
-  logger.info("Deleting old performance datas...");
-  await deleteOldPerformances(idsToDelete);
+  logger.info("[PROCESS] DB에 있는 오래된 공연 데이터 삭제");
+  if (idsToDelete.length > 0) {
+    logger.info(`[DB] 삭제할 데이터 개수: ${idsToDelete.length}개`);
+    // 내부에서 fallback 로직 실행
+    await deletePerformances(idsToDelete);
+  } else {
+    logger.info("[DB] 삭제할 데이터가 없음");
+  }
 
-  // 어제 이후 업데이트된 공연 데이터 가져오기
-  logger.info("Fetching updated datas...");
+  // 4) 기존에 저장된 공연둘 중 수정된 공연의 id 가져오기
+  logger.info("[PROCESS] DB에 있는 기존 공연들 중 수정된 공연 id 가져오기");
   const idsToUpdate = await getPerformanceIds(
     startDate,
     updateEndDate,
     afterDate,
   );
 
-  // isToUpdate와 isToInsert에 동일한 id를 가진 데이터가 존재할 수 있으므로 set으로 제외
+  // 5) isToUpdate와 isToInsert에 동일한 id를 가진 데이터가 존재할 수 있으므로 set으로 제외
   const idsToProcess = [...new Set([...idsToInsert, ...idsToUpdate])];
-  console.log(idsToProcess);
+  logger.info("[PROCESS] 가공해야 할 공연 id:", idsToProcess);
 
-  logger.info("Fetching Performance Details...");
-
+  // 6) id를 바탕으로 공연 상세 데이터 가져오기
+  logger.info("[PROCESS] 공연 상세 데이터 가져오기");
   return getPerformanceList(idsToProcess);
 };
--- packages/backend/application/use-cases/1_extract/modules/compareNewOld.ts
+++ packages/backend/application/use-cases/1_extract/modules/compareNewOld.ts
@@ -1,29 +1,11 @@
-import { APIError } from "@/shared/utils/error";
-import { getColumnData } from "../../../../infrastructure/external-api/supabase/database";
-import { sendSlackNotification } from "@/shared/utils/monitor";
+export const compareNewOld = (newIds: string[], dbIds: string[]) => {
+  const newIdsSet = new Set(newIds);
+  const dbIdsSet = new Set(dbIds);
 
-export const compareNewOld = async (newPerformances: string[]) => {
-  try {
-    const dbPerformances = await getColumnData(
-      "performances",
-      "performance_id",
-    );
+  // DB에는 있지만 새 공연 데이터에는 없는 id -> 삭제 대상
+  const idsToDelete = [...dbIds].filter((id) => !newIdsSet.has(id));
+  // 새 공연 데이터에는 있지만 DB에는 없는 id -> 삽입 대상
+  const idsToInsert = [...newIds].filter((id) => !dbIdsSet.has(id));
 
-    const newPerformancesSet = new Set(newPerformances);
-    const dbPerformancesSet = new Set(dbPerformances);
-
-    // DB에는 있지만 새 공연 데이터에는 없는 id -> 삭제 대상
-    const idsToDelete = [...dbPerformances].filter(
-      (id) => !newPerformancesSet.has(id),
-    );
-    // 새 공연 데이터에는 있지만 DB에는 없는 id -> 삽입 대상
-    const idsToInsert = [...newPerformances].filter(
-      (id) => !dbPerformancesSet.has(id),
-    );
-
-    return { idsToDelete, idsToInsert };
-  } catch (error) {
-    await sendSlackNotification("❌ [FETCH_FAIL] Old DB Data Fetch Failed");
-    throw new APIError("[FETCH_FAIL] Old DB Data Fetch Failed");
-  }
+  return { idsToDelete, idsToInsert };
 };
